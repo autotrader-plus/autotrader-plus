e68a5cc9109be13f79366a00ff7916123af42b8f
ameen <ameenparthab@gmail.com>
2021-10-20 11:36:42 -0400

    Touched up on the class connectAutoTraderDB 1. Created a constructor that establishes a connection to database 2. create a private method to establish connection 3. created two methods to retrieve information    a. one returns a string of a car    b. one returns the information of a car in dictionary

--- main/connectAutoTraderDB.java
+++ main/connectAutoTraderDB.java
@@ -1,13 +1,25 @@
 import java.sql.*;
+import java.util.HashMap;
+
 public class connectAutoTraderDB{
-    public static void main(String[] args) {
+
+    private final Statement objStatement;
+
+    public connectAutoTraderDB(){
+        this.objStatement = connectAutoTraderDB.establishConnection();
+    }
+
+    private static Statement establishConnection(){
+
         try{
 
-            try {
+            try{
                 Class.forName("com.mysql.cj.jdbc.Driver");
             }
+
             catch (ClassNotFoundException e) {
                 e.printStackTrace();
+
             }
 
             String dburl="jdbc:mysql://traderautoplusdb.cs3kq1tsgpar.ca-central-1.rds.amazonaws.com:3306/cars";
@@ -16,16 +28,59 @@ public class connectAutoTraderDB{
 
             Connection myConnection= DriverManager.getConnection(dburl, username, password);
 
-            Statement myStatement=myConnection.createStatement();
+            return myConnection.createStatement();
 
-            ResultSet myResultSet=myStatement.executeQuery("SELECT * FROM cars.AvailableCars;");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 
-            while(myResultSet.next()){
-                System.out.println("Car: " + myResultSet.getString("name"));
+    public String returnCarDetailsString(Integer car_id){
+
+        String returnString = "";
+
+        try{
+
+            assert this.objStatement != null;
+            ResultSet myResultSet=this.objStatement.executeQuery("SELECT * FROM cars.AvailableCars " +
+                    "WHERE id_car = " + car_id.toString() + ";");
+
+            if(myResultSet.next()) {
+                returnString = "Car: " + myResultSet.getString("name") + "\n" +
+                               "Cost: $" + myResultSet.getString("cost");
             }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        return returnString;
+    }
+
+    public HashMap<String, Object> returnCarDetails(Integer car_id){
+
+        HashMap<String, Object> returnMap = new HashMap<>();
+
+        try{
+
+            assert this.objStatement != null;
+            ResultSet myResultSet=this.objStatement.executeQuery("SELECT * FROM cars.AvailableCars " +
+                    "WHERE id_car = " + car_id.toString() + ";");
+
+            if(myResultSet.next()) {
+                returnMap.put("Car", myResultSet.getString("name"));
+                returnMap.put("Cost", myResultSet.getString("cost"));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return returnMap;
+    }
+
+    public static void main(String[] args) {
+        connectAutoTraderDB t = new connectAutoTraderDB();
+        System.out.println(t.returnCarDetailsString(1));
+        System.out.println(t.returnCarDetails(1).keySet());
     }
 }
