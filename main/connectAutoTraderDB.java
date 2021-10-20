5660244b8119f8d7f12cd623b8102491b0ac282e
ameen <ameenparthab@gmail.com>
2021-10-20 15:27:32 -0400

    Began documenting code

--- main/connectAutoTraderDB.java
+++ main/connectAutoTraderDB.java
@@ -3,6 +3,12 @@ import java.util.HashMap;
 
 public class connectAutoTraderDB{
 
+    /**
+     * Represents a connection to AutoTraderDB.
+     * Can call data in string form and Dictionary form
+    */
+
+    // This variable essentially represents the connection to the SQLDB; it holds information about that connection
     private final Statement objStatement;
 
     public connectAutoTraderDB(){
@@ -12,7 +18,6 @@ public class connectAutoTraderDB{
     private static Statement establishConnection(){
 
         try{
-
             try{
                 Class.forName("com.mysql.cj.jdbc.Driver");
             }
@@ -36,51 +41,47 @@ public class connectAutoTraderDB{
         return null;
     }
 
-    public String returnCarDetailsString(Integer car_id){
+    private ResultSet writeQuery(String query) throws SQLException {
+
+        return this.objStatement.executeQuery(query);
+
+    }
+
+    public String returnCarDetailsString(Integer car_id) throws SQLException {
+
+        String query = "SELECT * FROM cars.AvailableCars " +
+                "WHERE id_car = " + car_id.toString() + ";";
+
+        ResultSet myResultSet = writeQuery(query);
 
         String returnString = "";
-
-        try{
-
-            assert this.objStatement != null;
-            ResultSet myResultSet=this.objStatement.executeQuery("SELECT * FROM cars.AvailableCars " +
-                    "WHERE id_car = " + car_id.toString() + ";");
-
-            if(myResultSet.next()) {
-                returnString = "Car: " + myResultSet.getString("name") + "\n" +
-                               "Cost: $" + myResultSet.getString("cost");
-            }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        if(myResultSet.next()) {
+            returnString = "Car: " + myResultSet.getString("name") + "\n" +
+                           "Cost: $" + myResultSet.getString("cost");
         }
+
         return returnString;
     }
 
-    public HashMap<String, Object> returnCarDetails(Integer car_id){
+    public HashMap<String, Object> returnCarDetails(Integer car_id) throws SQLException {
+
+        String query = "SELECT * FROM cars.AvailableCars " +
+                "WHERE id_car = " + car_id.toString() + ";";
+
+        ResultSet myResultSet = writeQuery(query);
 
         HashMap<String, Object> returnMap = new HashMap<>();
-
-        try{
-
-            assert this.objStatement != null;
-            ResultSet myResultSet=this.objStatement.executeQuery("SELECT * FROM cars.AvailableCars " +
-                    "WHERE id_car = " + car_id.toString() + ";");
-
             if(myResultSet.next()) {
                 returnMap.put("Car", myResultSet.getString("name"));
                 returnMap.put("Cost", myResultSet.getString("cost"));
             }
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
         return returnMap;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         connectAutoTraderDB t = new connectAutoTraderDB();
-        System.out.println(t.returnCarDetailsString(1));
-        System.out.println(t.returnCarDetails(1).keySet());
+        System.out.println(t.returnCarDetailsString(2));
+        System.out.println(t.returnCarDetails(2).values());
     }
 }
