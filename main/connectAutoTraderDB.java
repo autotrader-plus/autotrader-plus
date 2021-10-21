d7224b1a160bd33865ce74ace2d28cc3e90c6308
Jia Hao <71251813+jiah29@users.noreply.github.com>
2021-10-21 00:31:51 -0400

    Resolved conflicts with ConnectDB branch

--- main/connectAutoTraderDB.java
+++ main/connectAutoTraderDB.java
@@ -1,15 +1,30 @@
-package main;
-
 import java.sql.*;
-public class connectAutoTraderDB{
-    public static void main(String[] args) {
-        try{
+import java.util.HashMap;
 
-            try {
+public class connectAutoTraderDB{
+
+    /**
+     * Represents a connection to AutoTraderDB.
+     * Can call data in string form and Dictionary form
+    */
+
+    // This variable essentially represents the connection to the SQLDB; it holds information about that connection
+    private final Statement objStatement;
+
+    public connectAutoTraderDB(){
+        this.objStatement = connectAutoTraderDB.establishConnection();
+    }
+
+    private static Statement establishConnection(){
+
+        try{
+            try{
                 Class.forName("com.mysql.cj.jdbc.Driver");
             }
+
             catch (ClassNotFoundException e) {
                 e.printStackTrace();
+
             }
 
             String dburl="jdbc:mysql://traderautoplusdb.cs3kq1tsgpar.ca-central-1.rds.amazonaws.com:3306/cars";
@@ -18,16 +33,55 @@ public class connectAutoTraderDB{
 
             Connection myConnection= DriverManager.getConnection(dburl, username, password);
 
-            Statement myStatement=myConnection.createStatement();
-
-            ResultSet myResultSet=myStatement.executeQuery("SELECT * FROM cars.AvailableCars;");
-
-            while(myResultSet.next()){
-                System.out.println("Car: " + myResultSet.getString("name"));
-            }
+            return myConnection.createStatement();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        return null;
+    }
+
+    private ResultSet writeQuery(String query) throws SQLException {
+
+        return this.objStatement.executeQuery(query);
+
+    }
+
+    public String returnCarDetailsString(Integer car_id) throws SQLException {
+
+        String query = "SELECT * FROM cars.AvailableCars " +
+                "WHERE id_car = " + car_id.toString() + ";";
+
+        ResultSet myResultSet = writeQuery(query);
+
+        String returnString = "";
+        if(myResultSet.next()) {
+            returnString = "Car: " + myResultSet.getString("name") + "\n" +
+                           "Cost: $" + myResultSet.getString("cost");
+        }
+
+        return returnString;
+    }
+
+    public HashMap<String, Object> returnCarDetails(Integer car_id) throws SQLException {
+
+        String query = "SELECT * FROM cars.AvailableCars " +
+                "WHERE id_car = " + car_id.toString() + ";";
+
+        ResultSet myResultSet = writeQuery(query);
+
+        HashMap<String, Object> returnMap = new HashMap<>();
+            if(myResultSet.next()) {
+                returnMap.put("Car", myResultSet.getString("name"));
+                returnMap.put("Cost", myResultSet.getString("cost"));
+            }
+
+        return returnMap;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        connectAutoTraderDB t = new connectAutoTraderDB();
+        System.out.println(t.returnCarDetailsString(2));
+        System.out.println(t.returnCarDetails(2).values());
     }
 }
