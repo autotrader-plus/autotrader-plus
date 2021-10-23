802f578c609d92ff86445dea9d7878a1b64440d8
JiaHao <71251813+jiah29@users.noreply.github.com>
2021-10-22 22:12:49 -0400

    add cors cross origin annotation

--- spring-boot/src/main/java/com/example/springboot/ReturnDatabaseInfo.java
+++ spring-boot/src/main/java/com/example/springboot/ReturnDatabaseInfo.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins ="*")
 public class ReturnDatabaseInfo {
 
     private final AtomicLong counter = new AtomicLong();
--- spring-boot/src/main/java/com/example/springboot/ReturnSensoAPI.java
+++ spring-boot/src/main/java/com/example/springboot/ReturnSensoAPI.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins ="*")
 public class ReturnSensoAPI {
 
     private final AtomicLong counter = new AtomicLong();
