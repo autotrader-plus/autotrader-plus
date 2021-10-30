a30f2ef04c4c8e78e01c835e5ecd6db06432c454
elizabethhli <elizabethyli@outlook.com>
2021-10-29 23:33:31 -0400

    Created 3 new tests for informationmanipulation and added more info to ReturnCarInformationTest.java -- todos remain to be completed

--- spring-boot/src/test/java/com/example/informationmanipulation/ReturnCarInformationTest.java
+++ spring-boot/src/test/java/com/example/informationmanipulation/ReturnCarInformationTest.java
@@ -1,8 +1,10 @@
 package com.example.informationmanipulation;
 
+import com.example.connectouterentity.ConnectAutoTraderDB;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Objects;
@@ -30,6 +32,31 @@ class ReturnCarInformationTest {
         testMap.put("Dealership", "Guelph Hyundai");
         testMap.put("Car Type", "Convertible");
         assert Objects.equals(testMap, ReturnCarInformation.returnCarDetails(1));
+    }
+
+    @Test
+    @DisplayName("PopulateCarMap: Basic Case")
+    void populateCarMap() throws SQLException {
+        // TODO: hardcode result set instead of establishing connection
+        //      Not sure what myResultSet should look like
+        // Writing a SQL query
+        String query = "SELECT * FROM cars.AvailableCars " +
+                "WHERE id_car = " + 1 + ";";
+
+        // Establish a connection and create a set of results from that query
+        ConnectAutoTraderDB connection = new ConnectAutoTraderDB();
+        ResultSet myResultSet = connection.writeQuery(query);
+
+        HashMap<String, String> testMap = new HashMap<>();
+        testMap.put("ID", "1");
+        testMap.put("Car", "Hyundai Sonata Ultimate");
+        testMap.put("Cost", "35649");
+        testMap.put("Mileage", "10323");
+        testMap.put("Model Year", "2021");
+        testMap.put("Dealership", "Guelph Hyundai");
+        testMap.put("Car Type", "Convertible");
+
+        assert Objects.equals(testMap, ReturnCarInformation.populateCarMap(myResultSet));
     }
 
 }
---
+++ spring-boot/src/test/java/com/example/informationmanipulation/ReturnUserInformationTest.java
@@ -0,0 +1,37 @@
+package com.example.informationmanipulation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReturnUserInformationTest {
+
+
+    @Test
+    @DisplayName("User: Basic Case")
+    void returnUser() throws SQLException {
+
+        Integer user_id = 1;
+
+        HashMap<String, String> testMap = new HashMap<>();
+        // TODO: Populate testMap with a user that exists in the database
+        testMap.put("ID", "");
+        testMap.put("Name", "");
+        testMap.put("Credit Score", "");
+        testMap.put("Location", "");
+        testMap.put("Max Downpayment", "");
+        testMap.put("Max Monthly Payment", "");
+        testMap.put("Monthly Income", "");
+        testMap.put("Employment Status", "");
+        testMap.put("Homeowner", "");
+        testMap.put("Monthly Debt Obligation", "");
+
+        assert Objects.equals(testMap, ReturnUserInformation.returnUser(user_id));
+    }
+
+}
