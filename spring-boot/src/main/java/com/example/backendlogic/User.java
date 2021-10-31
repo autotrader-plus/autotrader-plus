38b590d9d437c7d632410e6a54b438a2cdfe477d
Danx1129 <78716684+Danx1129@users.noreply.github.com>
2021-10-31 18:52:25 -0400

    Add files via upload

--- spring-boot/src/main/java/com/example/backendlogic/User.java
+++ spring-boot/src/main/java/com/example/backendlogic/User.java
@@ -5,47 +5,76 @@ public class User {
     private int monthlybudget;
     private int downpayment;
     private String zipcode;
+    private String name;
     private int monthlyincome;
     private boolean employed;
     private boolean homeowner;
     private int monthlydebt;
 
-    public User(int creditscore, int monthlybudget, int downpayment, String zipcode){
+    public User(int creditscore, int monthlybudget, int downpayment, String zipcode, String name){
         this.creditscore = creditscore;
         this.monthlybudget = monthlybudget;
         this.downpayment = downpayment;
         this.zipcode = zipcode;
+        this.name = name;
     }
 
-    public User(int creditscore, int monthlybudget, int downpayment, String zipcode, int monthlyincome,
-                 boolean employed, boolean homeowner, int monthlydebt){
+    public User(int creditscore, int monthlybudget, int downpayment, String zipcode, String name, int monthlyincome,
+                boolean employed, boolean homeowner, int monthlydebt){
         this.creditscore = creditscore;
         this.monthlybudget = monthlybudget;
         this.downpayment = downpayment;
         this.zipcode = zipcode;
+        this.name = name;
         this.monthlyincome = monthlyincome;
         this.employed = employed;
         this.homeowner = homeowner;
         this.monthlydebt = monthlydebt;
     }
 
-    public int getPriceRange(){
-        return (36 * this.monthlybudget) + this.downpayment;
+    public String getPriceRange(){
+        return Integer.toString((36 * this.monthlybudget) + this.downpayment);
     }
 
-    public int getPriceRange(int term){
-        return (term * this.monthlybudget) + this.downpayment;
+    public String getPriceRange(int term){
+        return Integer.toString((term * this.monthlybudget) + this.downpayment);
     }
 
-    public int getMonthlybudget(){
-        return this.monthlybudget;
+    public String getMonthlybudget(){
+        return Integer.toString(this.monthlybudget);
     }
 
-    public int getCreditscore(){
-        return this.creditscore;
+    public String getCreditscore(){
+        return Integer.toString(this.creditscore);
     }
 
     public String getLocation(){
         return this.zipcode;
+    }
+
+    public String getName() {  return this.name;
+    }
+
+    public String getDownpayment() { return Integer.toString(this.downpayment);
+    }
+
+    public String getMonthlyincome() { return Integer.toString(this.monthlyincome);
+    }
+
+    public String isEmployed() {
+        if (this.employed) {
+            return "Employed";
+        }
+        return "not Employed";
+    }
+
+    public String isHomeowner() {
+        if (this.homeowner){
+            return "Homeowner";
+        }
+        return "not Homeowner";
+    }
+
+    public String getMonthlydebt() { return Integer.toString(this.monthlydebt);
     }
 }
