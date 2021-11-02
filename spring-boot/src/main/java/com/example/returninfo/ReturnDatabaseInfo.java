42624638eaa412f2b510f0184785b523f87abf7f
JiaHao <71251813+jiah29@users.noreply.github.com>
2021-11-02 14:53:08 -0400

    Remove and refactor duplicate formatting files
    DETAILED DESCRIPTION
    --------------------
    Remove the SensoEndpointHandler because we don't need that endpoint anymore (its role is covered by the ServerMainEndpointHandler). I have also removed the duplicate formatting files.
    
    TESTING INFORMATION
    -------------------
    This change is not tested, because it is just removing files that are no longer needed (no other classes use them).

--- spring-boot/src/main/java/com/example/returninfo/ReturnDatabaseInfo.java
+++ spring-boot/src/main/java/com/example/returninfo/ReturnDatabaseInfo.java
@@ -30,9 +30,9 @@ public class ReturnDatabaseInfo {
 
     @CrossOrigin(origins = "http://ec2-18-118-163-255.us-east-2.compute.amazonaws.com:8080")
     @GetMapping("/database")
-    public HttpResponseDatabase httpResponseDatabase(@RequestParam(required = false, defaultValue = "") String name) throws SQLException{
+    public HttpResponseMain httpResponseMain(@RequestParam(required = false, defaultValue = "") String name) throws SQLException{
         System.out.println("==== Connecting to Autotrader Database ====");
-        HttpResponseDatabase db_response = new HttpResponseDatabase(counter.incrementAndGet(), getContent());
+        HttpResponseMain db_response = new HttpResponseMain(counter.incrementAndGet(), getContent());
         return db_response;
     }
 }
--- spring-boot/src/main/java/com/example/returninfo/ServerMainEndpointHandler.java
+++ spring-boot/src/main/java/com/example/returninfo/ServerMainEndpointHandler.java
@@ -11,7 +11,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
