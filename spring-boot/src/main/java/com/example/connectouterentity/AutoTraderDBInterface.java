c99570449014c5201508893ac4ea15bd488dcd69
ameen <ameenparthab@gmail.com>
2021-11-12 20:05:07 -0500

    Documentation of code
    Documented AutoTraderDBInterface

--- spring-boot/src/main/java/com/example/connectouterentity/AutoTraderDBInterface.java
+++ spring-boot/src/main/java/com/example/connectouterentity/AutoTraderDBInterface.java
@@ -4,7 +4,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public interface AutoTraderDBInterface {
-
+    /**
+     * An interface that prevents backend from directly interacting with database and database connectors.
+     */
     ResultSet writeQuery(String query) throws SQLException;
     void exceuteQuery(String query) throws SQLException;
 }
