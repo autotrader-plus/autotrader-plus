6cb80176e5ceb47acd941fc1d2322b312556186e
elizabethhli <elizabethyli@outlook.com>
2021-11-13 00:21:00 -0500

    Finished tests for CarTest and CarListTest -- still need to run

--- spring-boot/src/test/java/com/example/backendlogic/CarListTest.java
+++ spring-boot/src/test/java/com/example/backendlogic/CarListTest.java
@@ -1,20 +1,35 @@
 package com.example.backendlogic;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarListTest {
 
-    @Test
-    void addtoList() {
+    @BeforeEach
+    void setUp() {
+        CarList<Car> testCarList = new CarList<>();
+
+        Car c1 = new Car("2016", "Infiniti QX50", "70953", "Wagon",
+                "24", "25900", "Dixie Infiniti");
     }
 
     @Test
-    void size() {
+    void AddToList(CarList<Car> testCarList, Car c1) {
+        testCarList.AddtoList(c1);
+        assert testCarList.getCar(1).equals(c1);
     }
 
     @Test
-    void getCar() {
+    void size(CarList<Car> testCarList) {
+        assert testCarList.size() == 1;
+    }
+
+    @Test
+    void getCar(CarList<Car> testCarList, Car c1) {
+        assert testCarList.getCar(1).equals(c1);
     }
 }
--- spring-boot/src/test/java/com/example/backendlogic/CarTest.java
+++ spring-boot/src/test/java/com/example/backendlogic/CarTest.java
@@ -9,33 +9,42 @@ class CarTest {
 
     @BeforeEach
     void setUp() {
+        Car c1 = new Car("2016", "Infiniti QX50", "71935", "Wagon",
+                "24", "25895", "Dixie Infiniti");
+    }
+
+// Will implement test when method is used in the future
+//    @Test
+//    void CanAfford() {
+//    }
+
+    @Test
+    void getPrice(Car c1) {
+        assert c1.getPrice() == 25895;
     }
 
     @Test
-    void canAfford() {
+    void getBrand(Car c1) {
+        assert c1.getBrand().equals("Infiniti QX50");
     }
 
     @Test
-    void getPrice() {
+    void getYear(Car c1) {
+        assert c1.getYear().equals("2016");
     }
 
     @Test
-    void getBrand() {
+    void getKMS(Car c1) {
+        assert c1.getKMS().equals("71935");
     }
 
     @Test
-    void getYear() {
+    void returnID(Car c1) {
+        assert c1.returnID().equals("24");
     }
 
-    @Test
-    void getKMS() {
-    }
-
-    @Test
-    void returnID() {
-    }
-
-    @Test
-    void show() {
-    }
+// Will implement test when method is used in the future
+//    @Test
+//    void show() {
+//    }
 }
