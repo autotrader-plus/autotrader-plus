2a463a8490c558c7796264ca321530078a9e796e
JiaHao <71251813+jiah29@users.noreply.github.com>
2021-11-25 14:50:32 -0500

    Update ConnectSensoRateAPITest.java

--- spring-boot/src/test/java/packages/connectouterentity/ConnectSensoRateAPITest.java
+++ spring-boot/src/test/java/packages/connectouterentity/ConnectSensoRateAPITest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.HashMap;
 
-class ConnectSensoAPITest {
+class ConnectSensoRateAPITest {
 
     private ConnectSensoRateAPI connector;
 
