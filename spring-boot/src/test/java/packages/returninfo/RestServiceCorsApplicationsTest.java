75cdeb8ad9bcc7781d954b3cf15facbfbcad9f7c
JiaHao <71251813+jiah29@users.noreply.github.com>
2021-11-27 00:27:33 -0500

    Create a test for CORS Application

---
+++ spring-boot/src/test/java/packages/returninfo/RestServiceCorsApplicationsTest.java
@@ -0,0 +1,56 @@
+package packages.returninfo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**This class tests that the CORS Application can be started and run successfuly based on the root endpoint**/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("corsFilterBean")
+public class RestServiceCorsApplicationsTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void main() throws Exception {
+
+        MvcResult result = mvc
+                .perform(get("/"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        MockHttpServletResponse mockResponse = result.getResponse();
+
+        Collection<String> responseHeaders = mockResponse.getHeaderNames();
+        assertThat(responseHeaders).isNotNull();
+        assertThat(1).isEqualTo(1);
+        assertThat(responseHeaders.size()).isBetween(2, 15);
+    }
+
+}
