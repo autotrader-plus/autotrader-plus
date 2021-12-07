067fafc0141852764e131692fad122f850029053
danx1129 <danielxu1129@gmail.com>
2021-12-06 21:11:37 -0500

    Finished LoanResponseConstructorTest and LoanScoreCalculatorTest

--- src/test/java/packages/backend_logic/LoanScoreCalculatorTest.java
+++ src/test/java/packages/backend_logic/LoanScoreCalculatorTest.java
@@ -17,6 +17,7 @@ public class LoanScoreCalculatorTest {
     BasicLoans loan;
     LoansScoreCalculator score;
 
+
     @BeforeEach
     void setUp() throws SQLException, SensoConnectionFailureException {
         ReturnMultipleCars returnMultipleCars = new ReturnMultipleCars();
@@ -44,8 +45,36 @@ public class LoanScoreCalculatorTest {
         HashMap<String, String> score1 = score.getLoansScore1();
         HashMap<String, String> score2 = score.getLoansScore2();
         HashMap<String, String> score3 = score.getLoansScore3();
-        System.out.println(score1);
-        System.out.println(score2);
-        System.out.println(score3);
+
+        assert score1.size() == 1 && score1.containsKey("13");
+        assert score2.size() == 1 && score2.containsKey("13");
+        assert score3.size() == 0;
+    }
+
+    @Test
+    void getLoanTerm() throws SensoConnectionFailureException, IOException, InterruptedException{
+        HashMap<String, Object> output = loan.getLoans1();
+        HashMap<String, Object> output2 = loan.getLoans2();
+        HashMap<String, Object> output3 = loan.getLoans3();
+        score = new LoansScoreCalculator(userInfo, output, output2, output3, loan.getCars());
+        HashMap<String, Integer> loanTerm1 = score.getLoanTerm1();
+        HashMap<String, Integer> loanTerm2 = score.getLoanTerm2();
+        HashMap<String, Integer> loanTerm3 = score.getLoanTerm3();
+
+        assert loanTerm1.size() == 1 && loanTerm1.containsKey("13");
+        assert loanTerm2.size() == 1 && loanTerm2.containsKey("13");
+        assert loanTerm3.size() == 0;
+    }
+    @Test
+    void getCars() {
+        CarList cars = score.getCars();
+        Car car = (Car) cars.getCar(0);
+        assert car.getBrand().equals("Mercedes-Benz S-Class");
+    }
+
+    @Test
+    void getBuyer() {
+        User buyer = score.getUser();
+        assert buyer.getCreditScore().equals("770") && buyer.getName().equals("Bob Du");
     }
 }
